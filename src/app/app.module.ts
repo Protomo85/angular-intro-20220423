f52cfa140e87af7837255bbad1e449c24f3a9942
Protomo85 <tidziak@o2.pl>
2022-04-23 10:34:35 +0200

    Angular-intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { CounterComponent } from './counter/counter.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HelloComponent, CounterComponent, TodosComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
